38487ec8f6abe6d53c5a95b9707c97ca6ec8445a
Yogurt_with_thoughts <skrytnyjezik@gmail.com>
2024-10-03 09:27:50 +0300

    Добавьте файлы проекта.

---
+++ Core/MyNavigation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace amalgama_App.Core
+{
+    internal class MyNavigation
+    {
+        public static class CoreNavigate
+        {
+            public static Frame? NavigatorCore { get; set; }
+        }
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using amalgama_App.View.Pages;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static amalgama_App.Core.MyNavigation;
+
+namespace amalgama_App
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CoreNavigate.NavigatorCore = MainFrame;
+            MainFrame.Navigate(new StartPage());
+
+        }
+        private void Window_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+          
+            this.WindowState = WindowState.Maximized; 
+            this.ResizeMode = ResizeMode.NoResize;
+        }
+    }
+}
